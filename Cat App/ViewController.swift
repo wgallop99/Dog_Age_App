9f36d9f000f7f5f02437f325e9507aeea88821b8
Will Gallop <wgallop99@gmail.com>
2014-11-03 20:34:09 -0500

    app functionality done, pretty cool stuff

--- Cat App/ViewController.swift
+++ Cat App/ViewController.swift
@@ -9,7 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var dogsAge: UITextField!
 
+    @IBOutlet weak var message: UILabel!
+    
+    @IBAction func button(sender: AnyObject) {
+        
+        var age=dogsAge.text.toInt()
+        age=age!*7
+        
+        println(dogsAge.text)
+        
+        message.text="Your cat is \(age!) years old"
+        
+        //notice the / before age in the message.text also the exclamation point is used again
+        
+        //! exclamation point tells Xcode everything is cool
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
